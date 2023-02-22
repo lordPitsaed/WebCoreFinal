41008444486f1e3ea3029eb39cee2b3d137869ca
lordpitsaed <usakovd00@gmail.com>
2023-02-22 17:59:03 +0300

    .

--- src/js/events.js
+++ src/js/events.js
@@ -6,9 +6,13 @@ let asideMenu = document.querySelector('.aside_menu')
 let buttonCloseMenu = document.querySelector('.button-close-aside')
 let modalBlur = document.querySelector('.tablet-blur')
 
-let buttonCloseFeedback = document.querySelector('.modal__close_button')
-let buttonOpenFeedback = document.querySelectorAll('.button_repair')
-let feedbackMenu = document.querySelector('.modal')
+let buttonCloseFeedback = document.querySelector('.close_feedback')
+let buttonOpenFeedback = document.querySelectorAll('.button_chat')
+let feedbackMenu = document.querySelector('.feedback')
+
+let buttonCloseCall = document.querySelector('.close-call')
+let buttonOpenCall = document.querySelectorAll('.button_call')
+let callMenu = document.querySelector('.call')
 
 brandsShowMoreButton.addEventListener('click', () => {
   brandWrapper.classList.toggle('brands__wrapper--show_more')
@@ -23,27 +27,27 @@ brandsShowMoreButton.addEventListener('click', () => {
 
 buttonOpenMenu.addEventListener('click', () => {
   asideMenu.classList.toggle('aside_menu--show')
-  modalBlur.classList.toggle('tablet-blur--show')
+  modalBlur.classList.add('tablet-blur--show')
   document.body.style.overflow = 'hidden'
 })
 
 buttonCloseMenu.addEventListener('click', () => {
   asideMenu.classList.toggle('aside_menu--show')
-  modalBlur.classList.toggle('tablet-blur--show')
+  modalBlur.classList.remove('tablet-blur--show')
   document.body.style.overflow = 'auto'
 })
 
 buttonOpenFeedback.forEach(function (item) {
   item.addEventListener('click', () => {
     feedbackMenu.classList.toggle('modal--show')
-    modalBlur.classList.toggle('tablet-blur--show')
+    modalBlur.classList.add('tablet-blur--show')
     document.body.style.overflow = 'hidden'
   })
 })
 
 buttonCloseFeedback.addEventListener('click', () => {
   feedbackMenu.classList.toggle('modal--show')
-  modalBlur.classList.toggle('tablet-blur--show')
+  modalBlur.classList.remove('tablet-blur--show')
   document.body.style.overflow = 'auto'
 })
 
@@ -55,7 +59,24 @@ document.addEventListener('click', (evt) => {
     if (feedbackMenu.classList.contains('modal--show')) {
       feedbackMenu.classList.toggle('modal--show')
     }
+    if (callMenu.classList.contains('modal--show')) {
+      callMenu.classList.toggle('modal--show')
+    }
     document.body.style.overflow = 'auto'
-    modalBlur.classList.toggle('tablet-blur--show')
+    modalBlur.classList.remove('tablet-blur--show')
   }
+})
+
+buttonOpenCall.forEach(function (item) {
+  item.addEventListener('click', () => {
+    callMenu.classList.toggle('modal--show')
+    modalBlur.classList.add('tablet-blur--show')
+    document.body.style.overflow = 'hidden'
+  })
+})
+
+buttonCloseCall.addEventListener('click', () => {
+  callMenu.classList.toggle('modal--show')
+  modalBlur.classList.remove('tablet-blur--show')
+  document.body.style.overflow = 'auto'
 })
